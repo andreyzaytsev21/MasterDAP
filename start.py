60ec9630c47feeda009b24adb9671bb44c7a24ec
Andrey Zaytsev <zap-92@mail.ru>
2020-04-20 16:20:19 +0300

    unite windows

---
+++ start.py
@@ -0,0 +1,11 @@
+from files.EmployeeSelection import runEmployeeSelection
+from files.ManageDeals import runManageDeals
+
+#from files.SaveDeal import runSaveDeal
+
+
+
+#runEmployeeSelection()
+Employee = runEmployeeSelection()
+#runManageDeals()
+#runSaveDeal()
