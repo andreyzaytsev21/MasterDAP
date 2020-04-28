6b3f998c1fb2bd9fed0a1c202b5cb443d2d0bcef
konst-mois <konst-mois@yandex-team.ru>
2020-04-28 21:27:36 +0300

    Changes

--- src/EmployeeSelection.py
+++ src/EmployeeSelection.py
@@ -1,44 +1,71 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-import json
-from ManageDeals import runManageDeals
+
 from ResourcesProvider import ResourcesProvider
 
-def runEmployeeSelection(resourcesProvider: ResourcesProvider):
-    def buttonClickCallback():
-        chosenEmployeeOar = selectedEmployeeOar.get()
-        if chosenEmployeeOar == '':
-            messagebox.showerror("Ошибка", "Пользователь не выбран")
-        else:
-            tk.destroy()
-            runManageDeals(resourcesProvider, chosenEmployeeOar)
-    tk = Tk()
-    tk.title("МастерДАП")
-    w = tk.winfo_screenwidth()
-    h = tk.winfo_screenheight()
+
+def run_employee_selection(resources_provider: ResourcesProvider):
+    dialog = create_dialog("МастерДАП")
+
+    selected_employee_reference = StringVar()
+
+    create_label(dialog)
+    create_button(selected_employee_reference, dialog)
+
+    list_of_employees = load_employees(resources_provider)
+    create_combobox(selected_employee_reference, list_of_employees, dialog)
+
+    dialog.mainloop()
+
+    if not selected_employee_reference.get():
+        sys.exit()
+
+    return selected_employee_reference.get()
+
+
+def create_dialog(title: str):
+    dialog = Tk()
+    dialog.title(title)
+    w = dialog.winfo_screenwidth()
+    h = dialog.winfo_screenheight()
     w = w // 2
     h = h // 2
     w = w - 200
     h = h - 130
-    tk.geometry('400x200+{}+{}'.format(w, h))
+    dialog.geometry('400x200+{}+{}'.format(w, h))
 
-    label = Label(text='\n Выберите пользователя:\n', font=20)
+    return dialog
+
+
+def create_label(dialog: Tk):
+    label = Label(dialog, text='\n Выберите пользователя:\n', font=20)
     label.pack()
-    btn = Button(tk, text="Далее", height=2, width=20, command=buttonClickCallback)
-    btn.pack(side=BOTTOM)
 
-    selectedEmployeeOar = StringVar()
 
-    with open(resourcesProvider.getConfigPath(), 'r', encoding='utf-8') as f:
-        dapJson = json.load(f)
-    listOfEmployee = []
-    for l in dapJson['employeesOar'].keys():
-        listOfEmployee.append(l)
-
-    combobox = ttk.Combobox(tk, textvariable=selectedEmployeeOar, values= listOfEmployee, height = len(listOfEmployee))
+def create_combobox(selected_employee_reference: StringVar, list_of_employees: list, dialog: Tk):
+    combobox = ttk.Combobox(dialog,
+                            textvariable=selected_employee_reference,
+                            values=list_of_employees,
+                            height=len(list_of_employees))
     combobox['state'] = 'readonly'
     combobox.pack()
 
-    tk.mainloop()
-    return selectedEmployeeOar.get()
+
+def create_button(selected_employee_reference: StringVar, dialog: Tk):
+    def button_click_callback():
+        if not selected_employee_reference.get():
+            messagebox.showerror("Ошибка", "Пользователь не выбран")
+        else:
+            dialog.destroy()
+
+    btn = Button(dialog, text="Далее", height=2, width=20, command=button_click_callback)
+    btn.pack(side=BOTTOM)
+
+
+def load_employees(resources_provider: ResourcesProvider):
+    with open(resources_provider.getConfigPath(), 'r', encoding='utf-8') as f:
+        dap_json = json.load(f)
+
+    return [*dap_json['employeesOar']]
--- src/ManageDeals.py
+++ src/ManageDeals.py
@@ -1,14 +1,15 @@
+import json
+
 from PyQt5 import Qt
 from PyQt5.QtGui import QPalette, QColor
-import json
-from SaveDeal import runSaveDeal
-from ResourcesProvider import ResourcesProvider
+
 from CreateDocs import runCreateDocs
-from tkinter import *
+from ResourcesProvider import ResourcesProvider
+from SaveDeal import runSaveDeal
 
-def runManageDeals(resourcesProvider: ResourcesProvider, chosenEmployeeOar: str):
+
+def run_manage_deals(resourcesProvider: ResourcesProvider, chosenEmployeeOar: str):
     class Widget(Qt.QWidget):
-
 
         def __init__(self):
             super().__init__()
@@ -28,13 +29,13 @@ def runManageDeals(resourcesProvider: ResourcesProvider, chosenEmployeeOar: str)
             modeButton.setFixedHeight(30)
             modeButton.setFixedWidth(30)
             modeButton.setIcon(iconMode)
-            #modeButton.pressed.connect(self.poverhMode)
+            # modeButton.pressed.connect(self.poverhMode)
             layoutVertical.addWidget(createCaseButton)
-
 
             with open(resourcesProvider.getDealsPath(), 'r', encoding='utf-8') as f:
                 d = json.load(f)
-            labels = [""] + [""] + [""] + ["№ дела об АП"] + ["КоАП РФ"] + ["юридическое лицо"] + ["ИНН"] + ["юридический адрес"]
+            labels = [""] + [""] + [""] + ["№ дела об АП"] + ["КоАП РФ"] + ["юридическое лицо"] + ["ИНН"] + [
+                "юридический адрес"]
 
             win = Qt.QTableWidget(0, len(labels))
             win.setColumnHidden(10, False)
@@ -44,9 +45,9 @@ def runManageDeals(resourcesProvider: ResourcesProvider, chosenEmployeeOar: str)
             allItemsReversed = sorted(allItems, reverse=True)
 
             for i, (key, value) in enumerate(allItemsReversed):
-                rows = [""] + [""] + [""] + ["10418000-"+ key + "/2020"] + \
-                       [d[key]["codeRF_sh"]] + [d[key]["company"]["ul"]] + [d[key]["company"]["inn"]] +\
-                       [d[key]["company"]["address"]["ulIndex"] + ", " + d[key]["company"]["address"]["ulCity"] + ", " +\
+                rows = [""] + [""] + [""] + ["10418000-" + key + "/2020"] + \
+                       [d[key]["codeRF_sh"]] + [d[key]["company"]["ul"]] + [d[key]["company"]["inn"]] + \
+                       [d[key]["company"]["address"]["ulIndex"] + ", " + d[key]["company"]["address"]["ulCity"] + ", " + \
                         d[key]["company"]["address"]["ulStreetOffice"]]
                 win.insertRow(win.rowCount())
                 buttonCreate = Qt.QPushButton()
@@ -57,7 +58,7 @@ def runManageDeals(resourcesProvider: ResourcesProvider, chosenEmployeeOar: str)
                 buttonEdit.setIcon(iconEdit)
                 win.setCellWidget(i, 1, buttonEdit)
                 buttonDelete = Qt.QPushButton()
-                #buttonDelete.pressed.connect(lambda chosenDeal=key: self.goToDeleteDeal(chosenDeal))
+                # buttonDelete.pressed.connect(lambda chosenDeal=key: self.goToDeleteDeal(chosenDeal))
                 buttonDelete.setIcon(iconDelete)
                 win.setCellWidget(i, 2, buttonDelete)
 
@@ -70,8 +71,8 @@ def runManageDeals(resourcesProvider: ResourcesProvider, chosenEmployeeOar: str)
             layoutVertical.addWidget(win)
             layoutVertical.addWidget(modeButton)
 
-        #def poverhMode(app):
-         #   workWin.wm_attributes('-topmost',1)
+        # def poverhMode(app):
+        #   workWin.wm_attributes('-topmost',1)
 
         def goToRunSaveDeal(self):
             runSaveDeal(resourcesProvider)
@@ -79,16 +80,14 @@ def runManageDeals(resourcesProvider: ResourcesProvider, chosenEmployeeOar: str)
         def goToFormDocs(self, chosenDeal, chosenEmployeeOar):
             runCreateDocs(resourcesProvider, chosenDeal, chosenEmployeeOar)
 
+        # def goToEditDeal(self):
+        # runEditDeal(resourcesProvider, chosenDeal)
 
-        #def goToEditDeal(self):
-            #runEditDeal(resourcesProvider, chosenDeal)
+        # def goToDeleteDeal(self, chosenDeal):
 
-        #def goToDeleteDeal(self, chosenDeal):
+        # runDeleteDeal(resourcesProvider, chosen Deal)
 
-            #runDeleteDeal(resourcesProvider, chosen Deal)
-
-
-    #if __name__ == '__main__':
+    # if __name__ == '__main__':
     app = Qt.QApplication([])
     workWin = Widget()
     workWin.setWindowTitle("МастерДАП / Приволжская электронная таможня - ОАР - " + chosenEmployeeOar)
